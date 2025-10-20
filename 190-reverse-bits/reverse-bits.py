d5be5085ccc0f205330fc843c64537e8bbdacf6d
szymonwas <111058301+Wasumaster@users.noreply.github.com>
2025-10-21 01:31:04 +0200

    Time: 35 ms (80.61%) | Memory: 17.7 MB (68.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,8 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        binary_str = bin(n)[2:]
-        padded_str = binary_str.zfill(32)
-        flipped_str = padded_str[::-1]
-        result = int(flipped_str, 2)
-    
-        return result
+        
+        res = 0
+        for i in range(32):
+            last_bit = n & 1
+            res = res << 1
+            res = res | last_bit
+
+            n = n >> 1
+        return res
