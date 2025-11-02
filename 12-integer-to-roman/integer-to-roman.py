6140bf902b49e34554891aae2cc2ce055d3441e8
szymonwas <111058301+Wasumaster@users.noreply.github.com>
2025-11-02 22:48:02 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.75%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        res = ''
+        roman_map = [
+            (1000, 'M'),
+            (900, 'CM'),
+            (500, 'D'),
+            (400, 'CD'),
+            (100, 'C'),
+            (90, 'XC'),
+            (50, 'L'),
+            (40, 'XL'),
+            (10, 'X'),
+            (9, 'IX'),
+            (5, 'V'),
+            (4, 'IV'),
+            (1, 'I')
+        ]
+        for val, sym in roman_map:
+            while num >= val:
+                res += sym
+                num -= val
+        return res
