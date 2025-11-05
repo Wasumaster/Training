9cb2bafba0ecc972a12d05ecdebeaf51e164d259
szymonwas <111058301+Wasumaster@users.noreply.github.com>
2025-11-05 12:45:41 +0100

    Time: 3 ms (81.14%) | Memory: 18.5 MB (89.87%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -6,7 +6,8 @@ class Solution:
             current_sum = numbers[pointer_front] + numbers[pointer_back]
             if current_sum == target:
                 return [pointer_front + 1, pointer_back + 1]
-            elif current_sum > target:
-                pointer_back -= 1
-            else:
-                pointer_front += 1
+            else: 
+                if current_sum > target:
+                    pointer_back -= 1
+                else:
+                    pointer_front += 1
