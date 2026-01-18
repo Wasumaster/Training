cc3a1446bb4d148455205aa7e98905244796449d
szymonwas <111058301+Wasumaster@users.noreply.github.com>
2026-01-18 23:37:55 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.98%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,18 +1,19 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool: 
-        slowa_to_char = {}
+    def wordPattern(self, pattern: str, s: str) -> bool:
         char_to_slowa = {}
+        slowa_to_char = {}
         slowa = s.split()
         if len(slowa) != len(pattern):
             return False
 
-        for char, word in zip(pattern, slowa):
+        for char,word in zip(pattern,slowa):
             if char in char_to_slowa:
                 if char_to_slowa[char] != word:
                     return False
+            
             if word in slowa_to_char:
                 if slowa_to_char[word] != char:
-                    return False 
+                    return False
             
             slowa_to_char[word] = char
             char_to_slowa[char] = word
