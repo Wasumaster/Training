02118fc79b3ff2ff591942bed5210623b820f7c8
szymonwas <111058301+Wasumaster@users.noreply.github.com>
2026-01-28 01:20:23 +0100

    Time: 23 ms (50.90%) | Memory: 19.7 MB (29.28%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,21 +2,20 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         dictransom = {}
         dictmag = {}
-        for s in ransomNote:
-            if s in dictransom:
-                dictransom[s] += 1
+        for c in ransomNote:
+            if c in dictransom:
+                dictransom[c] += 1
             else:
-                dictransom[s] = 1
-
-        for s in magazine:
-            if s in dictmag:
-                dictmag[s] += 1
+                dictransom[c] = 1
+        
+        for c in magazine:
+            if c in dictmag:
+                dictmag[c] += 1
             else:
-                dictmag[s] = 1
-        for litera, ilosc_potrzebna in dictransom.items():
-
-            ilosc_dostepna = dictmag.get(litera, 0)
-
-            if ilosc_dostepna < ilosc_potrzebna:
+                dictmag[c] = 1
+        for key in dictransom:
+            if key in dictmag and dictmag[key] >= dictransom[key] :
+                continue
+            else:
                 return False
-        return True
+        return True 
